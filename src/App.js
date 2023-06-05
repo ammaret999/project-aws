d9fab0bf1cc2fdc96fbbe50c1db9bf0e5340d661
kao <ammaret.a@appman.co.th>
2023-06-06 04:06:05 +0700

    page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-2"></div>
+
+      <div className="col-span-8 justify-center gap-4 shadow-xl bg-gray-50 mt-5">
+        <div className="flex justify-center mt-5">
+          <p className="text-2xl">Create Name</p>
+        </div>
+
+        <form>
+          <div className="grid gap-6 mb-6 grid-cols-1 m-4">
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Name
+              </label>
+              <input
+                type="text"
+                name="topic"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Name"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-12 gap-4 mb-3">
+            <div className="col-span-12 justify-items-center grid">
+              <button className="btn" type="submit">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="col-span-2"></div>
     </div>
   );
 }
